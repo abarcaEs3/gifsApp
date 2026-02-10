0f10dfc5c8ef375b2f33e738abe25896e424e601
Carlos Abarca <abarcaespinozacarlos@gmail.com>
2026-02-10 16:23:59 -0600

    RouterLinks - From secondary components

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  icon: string,
+  label: string,
+  route: string,
+  subLabel: string;
+}
 
 @Component({
   selector: 'app-gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
 })
-export class GifsSideMenuOptionsComponent { }
+export class GifsSideMenuOptionsComponent {
+  menuOptions : MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ]
+ }
